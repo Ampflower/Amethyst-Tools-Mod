194fab6881252013f2e05e3eb8c0880ccb068eb8
Ampflower <ampflower@outlook.com>
2023-01-31 23:47:20 -0600

    Pull ReloadListener into inner class

--- Fabric/src/main/java/com/macaronsteam/amethysttoolsmod/fabric/AmethystClientMain.java
+++ Fabric/src/main/java/com/macaronsteam/amethysttoolsmod/fabric/AmethystClientMain.java
@@ -39,26 +39,28 @@ public class AmethystClientMain implements ClientModInitializer {
         ItemsInit.ITEM_AMETHYST_TRIDENT.ifPresent(item -> {
             AccessorItemProperties.invokeRegister(item, new ResourceLocation("throwing"), (itemstack, level, livingentity, i) -> livingentity != null && livingentity.isUsingItem() && livingentity.getUseItem() == itemstack ? 1.0F : 0.0F);
 
-            ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new IdentifiableResourceReloadListener() {
-                private static final ResourceLocation fabricId = ResourceLocation.tryBuild(Constants.MODID, "amethyst_trident");
-
-                @Override
-                public ResourceLocation getFabricId() {
-                    return fabricId;
-                }
-
-                @Override
-                public Collection<ResourceLocation> getFabricDependencies() {
-                    return Collections.singleton(ResourceReloadListenerKeys.MODELS);
-                }
-
-                @Override
-                public CompletableFuture<Void> reload(final PreparationBarrier preparationBarrier, final ResourceManager resourceManager, final ProfilerFiller profilerFiller, final ProfilerFiller profilerFiller2, final Executor executor, final Executor executor2) {
-                    return AmethystTridentBEWLR.INSTANCE.reload(preparationBarrier, resourceManager, profilerFiller, profilerFiller2, executor, executor2);
-                }
-            });
+            ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new AmethystTridentReloadListener());
             BuiltinItemRendererRegistry.INSTANCE.register(item, AmethystTridentBEWLR.INSTANCE::renderByItem);
         });
         ItemsInit.ITEM_AMETHYST_TIPPED_ARROW.ifPresent(item -> ColorProviderRegistry.ITEM.register(AmethystItemColors.AMETHYST_TIPPED_ARROW, item));
+    }
+
+    private static class AmethystTridentReloadListener implements IdentifiableResourceReloadListener {
+        private static final ResourceLocation fabricId = ResourceLocation.tryBuild(Constants.MODID, "amethyst_trident");
+
+        @Override
+        public ResourceLocation getFabricId() {
+            return fabricId;
+        }
+
+        @Override
+        public Collection<ResourceLocation> getFabricDependencies() {
+            return Collections.singleton(ResourceReloadListenerKeys.MODELS);
+        }
+
+        @Override
+        public CompletableFuture<Void> reload(final PreparationBarrier preparationBarrier, final ResourceManager resourceManager, final ProfilerFiller profilerFiller, final ProfilerFiller profilerFiller2, final Executor executor, final Executor executor2) {
+            return AmethystTridentBEWLR.INSTANCE.reload(preparationBarrier, resourceManager, profilerFiller, profilerFiller2, executor, executor2);
+        }
     }
 }
